afb371244d46d664f8d983102b1a60b1a650277a
Christian Toney <christian.toney@beastslash.com>
2025-05-26 18:18:16 -0400

    update config

---
+++ src/init.lua
@@ -0,0 +1,31 @@
+--!strict
+
+local IEffect = require(script.Parent["effect-types"]);
+
+type Effect = IEffect.Effect;
+type Bounds = IEffect.Bounds;
+type RunEffectFunction = IEffect.RunEffectFunction;
+type FitFunction = IEffect.FitFunction;
+
+local Effect = {};
+
+export type ConstructorProperties = {
+  name: string;
+  run: RunEffectFunction;
+  fit: FitFunction;
+}
+
+function Effect.new(properties: ConstructorProperties): Effect
+
+  local effect: Effect = {
+    type = "Effect";
+    name = properties.name;
+    fit = properties.fit;
+    run = properties.run;
+  };
+
+  return effect;
+
+end;
+
+return Effect;
